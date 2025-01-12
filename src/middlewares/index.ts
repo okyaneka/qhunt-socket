48c9701cd7df6c20eec5d7028a0d85728f1ed60d
Oky Aneka <okyaneka@gmail.com>
2025-01-12 23:46:51 +0700

    feat: remove LogMiddleware and update exports in index

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,5 @@
 import AuthMiddleware from "./AuthMiddleware";
-import LogMiddleware from "./LogMiddleware";
 
-export { AuthMiddleware, LogMiddleware };
+export { AuthMiddleware };
 
-export default { AuthMiddleware, LogMiddleware };
+export default { AuthMiddleware };

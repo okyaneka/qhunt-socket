8ca3cd45ed341c19e36db5b254746a82d7522d82
Oky Aneka <okyaneka@gmail.com>
2025-01-12 19:38:12 +0700

    feat: add mongoose connection plugin for MongoDB integration

---
+++ src/plugins/mongoose.ts
@@ -0,0 +1,15 @@
+import { mongoose as db } from "qhunt-lib";
+import { env } from "~/configs";
+
+const mongoose = async () => {
+  try {
+    const uri = env.MONGO_URI;
+    await db.connect(uri, { dbName: env.MONGO_DB_NAME });
+    console.log("MongoDB connected successfully!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+export default mongoose;

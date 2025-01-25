3257fddc887e0cf45a7475990d9357615903489d
Oky Aneka <okyaneka@gmail.com>
2025-01-25 14:22:16 +0700

    feat: enhance socket logging and add ping-pong functionality

--- src/helpers/socket/index.ts
+++ src/helpers/socket/index.ts
@@ -6,15 +6,23 @@ const listen = (ops?: ConnectionHandler): ConnectionHandler => {
     log.info(socket, "connect");
 
     socket.onAny((event, ...args) => {
-      log.info(socket, `${event} ${JSON.stringify(args)}`);
+      const message = args
+        .map((v) => JSON.stringify(v))
+        .join(" ")
+        .trim();
+      log.info(socket, `${event} ${message}`);
     });
 
     socket.on("disconnect", (reason) => {
-      log.info(socket, `disconnect: ${reason}`);
+      log.info(socket, `disconnect ${reason}`);
     });
 
     socket.on("disconnecting", (reason) => {
-      log.info(socket, `disconnecting: ${reason}`);
+      log.info(socket, `disconnecting ${reason}`);
+    });
+
+    socket.on("ping", () => {
+      socket.emit("pong");
     });
 
     ops && ops(socket);

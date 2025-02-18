07ebebb38bf06bdf91c433a3ef1a11aebffa27c8
Oky Aneka <okyaneka@gmail.com>
2025-02-18 20:25:11 +0700

    feat(socket): refactor socket initialization to use dynamic namespace handling

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -2,13 +2,18 @@ import { Server } from "socket.io";
 import { AuthMiddleware } from "~/middlewares";
 import TriviaSocket from "./trivia";
 import LeaderboardSocket from "./leaderboard";
+import PhotohuntSocket from "./photohunt";
+
+const socketNamespaces = [
+  { namespace: "/leaderboard", handler: LeaderboardSocket },
+  { namespace: "/trivia", handler: TriviaSocket },
+  { namespace: "/photohunt", handler: PhotohuntSocket },
+];
 
 const sockets = (app: Server) => {
-  app
-    .of("/leaderboard")
-    .use(AuthMiddleware)
-    .on("connection", LeaderboardSocket);
-  app.of("/trivia").use(AuthMiddleware).on("connection", TriviaSocket);
+  socketNamespaces.forEach(({ namespace, handler }) => {
+    app.of(namespace).use(AuthMiddleware).on("connection", handler);
+  });
 };
 
 export default sockets;

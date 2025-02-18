7f6a766f4934e804a858f39bbe5eb51907478e60
Oky Aneka <okyaneka@gmail.com>
2025-02-18 18:33:25 +0700

    feat(socket): implement SocketManager with event listeners and error handling

--- src/helpers/types/index.ts
+++ src/helpers/types/index.ts
@@ -1,5 +1,9 @@
 import { ExtendedError, Socket } from "socket.io";
 
+export type Handler<T extends any[] = any[]> = (
+  ...args: T
+) => Promise<void> | void;
+
 export type ConnectionHandler = (socket: Socket) => Promise<void>;
 
 export type SocketHandler = (

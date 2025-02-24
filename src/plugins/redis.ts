ee90a4462cd555c9e1e3b25663e051e1fe674216
Oky Aneka <okyaneka@gmail.com>
2025-02-24 22:17:13 +0700

    refactor(redis): replace ioredis with qhunt-lib RedisHelper

--- src/plugins/redis.ts
+++ src/plugins/redis.ts
@@ -1,21 +1,18 @@
-import Redis from "ioredis";
 import { env } from "~/configs";
+import RedisHelper, { RedisOptions } from "qhunt-lib/plugins/redis";
 
-let instance: Redis;
+const options: RedisOptions = {
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  password: env.REDIS_PASSWORD,
+  retryStrategy: (times) => Math.min(times * 50, 2000),
+};
 
-const useRedis = () => {
-  if (!instance) {
-    instance = new Redis({
-      host: env.REDIS_HOST,
-      port: env.REDIS_PORT,
-      password: env.REDIS_PASSWORD,
-      retryStrategy: (times) => Math.min(times * 50, 2000),
-    });
+let instance: RedisHelper;
 
-    instance.on("connect", () => console.log("Redis connected successfully!"));
-    instance.on("error", (err) => console.error("❌ Redis Error:", err));
-  }
+const redis = () => {
+  if (!instance) instance = new RedisHelper(options);
   return instance;
 };
 
-export default useRedis;
+export default redis;
